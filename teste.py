6fe6402907ac71d3a4cc283a3f6d03cdf6b2efa2
Bruno Makhohl <brunomakhl@hotmail.com>
2020-10-07 23:59:46 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,21 @@
+test = 'This movie is not so bad'
+
+# bom = 'good'
+# nao = test.rsplit().index('not')
+# ruim = test.rsplit().index('bad')
+# if nao < ruim:
+#     print(test.strip(str(test.rsplit()[nao : ruim + 1])))
+
+# print(test.find('not'))
+# print(test.find('bad'))
+
+# bom = 'good'
+# nao = test.find('not')
+# ruim = test.find('bad')
+# if nao < ruim:
+#     new_text = test.replace('not', bom)
+#     print(new_text)
+# else:
+#     print('no')
+
+print(test.find('not')+3)
