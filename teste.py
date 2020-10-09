57bc1cccd692c96cb41e68c257b8095dc1e1ab1f
Bruno Makhohl <brunomakhl@hotmail.com>
2020-10-09 12:28:16 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,21 +1,55 @@
-test = 'This movie is not so bad'
-
-# bom = 'good'
-# nao = test.rsplit().index('not')
-# ruim = test.rsplit().index('bad')
-# if nao < ruim:
-#     print(test.strip(str(test.rsplit()[nao : ruim + 1])))
-
-# print(test.find('not'))
-# print(test.find('bad'))
-
-# bom = 'good'
-# nao = test.find('not')
-# ruim = test.find('bad')
-# if nao < ruim:
-#     new_text = test.replace('not', bom)
-#     print(new_text)
+#### ex 06
+# a = 'abcde'
+# b = 'xyz'
+#
+# # print(len(a))
+# # print(len(b))
+#
+# moda = len(a) % 2
+# sizea = len(a) / 2
+# modb = len(b) % 2
+# sizeb = len(b) / 2
+#
+# if moda != 0:
+#     inia = int(sizea+1)
+#     fima = int(sizea)
 # else:
-#     print('no')
+#     inia, fima = int(sizea), int(sizea)
+#
+# if modb != 0:
+#     inib = int(sizeb+1)
+#     fimb = int(sizeb)
+# else:
+#     inib, fimb = int(sizeb), int(sizeb)
+#
+# firsta = a[:inia]
+# lasta = a[fima:]
+# firstb = b[:inib]
+# lastb =  b[fimb:]
+#
+# print(firsta)
+# print(firstb)
+# print(lasta)
+# print(lastb)
+#
+# c = firsta+firstb+lasta+lastb
+#
+# print(c)
 
-print(test.find('not')+3)
+##################################################################
+
+#### ex 08
+
+words = ['aba', 'xyz', 'aa', 'x', 'bbb']
+i = 0
+itens_iguais = []
+
+while i < len(words):
+    a = words[i]
+    if len(a) >= 2 and a[0] == a[-1]:
+        itens_iguais.append(a)
+    else:
+        pass
+    i += 1
+
+print(len(itens_iguais))
