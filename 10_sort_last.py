55dd345282e2d3f879b258245b40f5565cdc6e86
Bruno Makhohl <brunomakhl@hotmail.com>
2020-10-14 23:54:28 -0300

    Update 10_sort_last.py

--- 10_sort_last.py
+++ 10_sort_last.py
@@ -11,7 +11,8 @@ Dica: Use uma custom key= function para extrair o ultimo elemento de cada tupla.
 """
 def sort_last(tuples):
     # +++ SUA SOLUÇÃO +++
-    return
+    good_list = sorted(tuples, key=lambda x: x[-1])
+    return good_list
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
